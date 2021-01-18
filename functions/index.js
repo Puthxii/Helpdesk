f146fc1c075900ce3554278a60f9efb721b25c87
Teenapat <teenapat.wu@gmail.com>
2021-01-19 04:48:33 +0700

    !SENDMAILER

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,28 @@
-const functions = require("firebase-functions");
-const admin=require('firebase-admin')
-
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const nodemailer = require('nodemailer')
 admin.initializeApp()
-require('dotenv').config
+require('dotenv').config()
 
-const {SENDER_EMAIL, SENDER_PASSWORD} = process.env;
+const { SENDER_EMAIL, SENDER_PASSWORD } = process.env
 
-// exports.sendEmailNotification=function.firestore.document('us')
+exports.sendEmailNotification = functions.firestore.document('ticket/{docId}')
+    .onCreate((snap, ctx) => {
+        const data = snap.data()
+        let authData = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: SENDER_EMAIL,
+                pass: SENDER_PASSWORD
+            }
+        })
+        authData.sendMail({
+            from: 'teenaputh12@gmail.com',
+            to: `${data.email}`,
+            subject: 'Your submission Ticket',
+            text: `${data.email}`,
+            html: `${data.email}`,
+        }).then(res => console.log('successfully sent that mail')).catch(err => console.log(err))
+    })
