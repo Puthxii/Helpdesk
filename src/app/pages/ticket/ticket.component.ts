ad7208dfebef778bdbb9fe8a22ca9cd947438135
tixxff <awatif5166@gmail.com>
2021-01-20 04:43:12 +0700

    fix layout

--- src/app/pages/ticket/ticket.component.ts
+++ src/app/pages/ticket/ticket.component.ts
@@ -43,6 +43,7 @@ export class TicketComponent implements OnInit {
     { value: 'More Info' },
     { value: 'Pending' },
     { value: 'Resolved' },
+    { value: 'Reject'},
     { value: 'Close' }
   ]
   ActiveStatus = [
@@ -50,7 +51,7 @@ export class TicketComponent implements OnInit {
     { name: 'Pending', icon: 'fas fa-clock mx-2' },
     { name: 'Close', icon: 'fas fa-check-circle mx-2' },
     { name: 'Reject', icon: 'fas fa-times-circle mx-2'},
-    { name: 'More Info', icon: 'fas fa-file mx-2'}
+    { name: 'More Info', icon: 'fas fa-file mx-2'},
   ]
   CountStatus = []
 
