62a2c312d5826dd92bdb7bf6f29876c1a9ad476a
Teenapat <teenapat.wu@gmail.com>
2021-02-05 14:36:33 +0700

    track with id

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,7 @@ const {SENDER_EMAIL, SENDER_PASSWORD} = process.env;
 exports.sendEmailNotification = functions.firestore.document("ticket/{docId}")
     .onCreate((snap, ctx) => {
         const data = snap.data();
+        const id = snap.id;
         const authData = nodemailer.createTransport({
             host: "smtp.gmail.com",
             port: 465,
@@ -22,8 +23,8 @@ exports.sendEmailNotification = functions.firestore.document("ticket/{docId}")
             from: "teenaputh12@gmail.com",
             to: `${data.email}`,
             subject: "Helpdesk - Ticket",
-            text: "Your ticket reported was successful. You can tracking your ticket by this link http://localhost:4200/site-ticket/${docId}",
-            html: "Your ticket reported was successful. You can tracking your ticket by this link http://localhost:4200/site-ticket/${docId}",
+            text: `${id}`,
+            html: `Your ticket reported was successful. You can tracking your ticket by this link http://localhost:4200/site-ticket/${id}`,
         }).then((res) => console.log("successfully sent that mail"))
             .catch((err) => console.log(err));
     });
