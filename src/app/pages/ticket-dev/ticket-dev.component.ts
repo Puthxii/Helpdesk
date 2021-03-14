97afa7cc38054bf2859bc51b9d6192682e62d7b4
Teenapat <teenapat.wu@gmail.com>
2021-03-14 13:54:53 +0700

    fix bug role

--- src/app/pages/ticket-dev/ticket-dev.component.ts
+++ src/app/pages/ticket-dev/ticket-dev.component.ts
@@ -90,7 +90,7 @@ export class TicketDevComponent implements OnInit {
   getCurrentUserByRoles() {
     this.userService.getUserbyId(this.User.uid).snapshotChanges().subscribe(data => {
       this.user$ = data.payload.data() as User
-      if (this.user$.roles.supporter === true) {
+      if (this.user$.roles.developer === true) {
         this.currentName = this.user$.firstName + ' ' + this.user$.lastName
         this.getCountByStatusCurrentname()
         this.getCountToltalCurrentname()
