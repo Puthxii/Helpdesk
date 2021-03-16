2732e191267605d9e8fc3778e6aceb99f094e47c
Teenapat <teenapat.wu@gmail.com>
2021-03-17 00:56:41 +0700

    🎨 refactor

--- src/app/guards/ma.guard.ts
+++ src/app/guards/ma.guard.ts
@@ -4,22 +4,22 @@ import { Observable } from 'rxjs';
 import { take, map, tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth/auth.service';
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 
 export class MaintenanceGuard implements CanActivate {
-    constructor(private auth: AuthService, private router: Router) { }
-    canActivate(
-        next: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot): Observable<boolean> {
-        return this.auth.user$.pipe(
-            take(1),
-            map(user => user && user.roles.maintenance ? true : false),
-            tap(isMaintenance => {
-                if (!isMaintenance) {
-                    this.router.navigate(['/error-pages/404']);
-                }
-            })
-        );
-    }
+  constructor(private auth: AuthService, private router: Router) { }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> {
+    return this.auth.user$.pipe(
+      take(1),
+      map(user => user && user.roles.maintenance ? true : false),
+      tap(isMaintenance => {
+        if (!isMaintenance) {
+          this.router.navigate(['/error-pages/404']);
+        }
+      })
+    );
+  }
 }
