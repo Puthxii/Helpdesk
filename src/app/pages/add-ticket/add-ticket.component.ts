117f6569325fd0f7d128c3118fed654c8d242049
Teenapat <teenapat.wu@gmail.com>
2021-03-30 10:39:50 +0700

    🎨 make code shorter

--- src/app/pages/add-ticket/add-ticket.component.ts
+++ src/app/pages/add-ticket/add-ticket.component.ts
@@ -448,12 +448,8 @@ export class AddTicketComponent implements OnInit {
     let month = '' + (date.getMonth() + 1);
     let day = '' + date.getDate();
     const year = date.getFullYear();
-    if (month.length < 2) {
-      month = '0' + month;
-    }
-    if (day.length < 2) {
-      day = '0' + day;
-    }
+    if (month.length < 2) { month = '0' + month; }
+    if (day.length < 2) { day = '0' + day; }
     return [day, month, year].join('/');
   }
 
