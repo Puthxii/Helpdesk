8a0d482f1496fd54568397feb4481a32254ffab9
Teenapat <teenapat.wu@gmail.com>
2021-04-09 14:25:24 +0700

    🔨 romove assign in model

--- src/app/models/ticket.model.ts
+++ src/app/models/ticket.model.ts
@@ -16,7 +16,6 @@ export interface Ticket {
     staff?: string;
     action?: Actions[];
     email?: string;
-    assign?: string;
     actionSentence?: string;
     participant?: any;
     responseDescription: string;
@@ -30,15 +29,17 @@ export interface Ticket {
     task?: Tasks[]
 }
 export interface Actions {
-    data: any,
-    status: string,
-    staff: string
+    actionSentence: string
+    dev?: any
+    staff?: string
+    status: string
+    date: any
 }
 
 export interface Tasks {
     id?: any
     subjectTask: string
-    developer: any,
-    point: number,
+    developer: any
+    point: number
     dueDate: any
 }
