5dfeb6c08e07c2b0471ba07df1498c1a80376ec2
Teenapat <teenapat.wu@gmail.com>
2021-07-27 17:39:20 +0700

    add site

--- src/app/models/site.model.ts
+++ src/app/models/site.model.ts
@@ -1,18 +1,29 @@
 import { Product } from './product.model';
+
+
 export interface Site {
-    $key: string;
-    sid: string;
-    initials: string;
-    nameEN: string;
-    nameTH: string;
-    productId: string;
-    maLevelId: string;
-    product: Product;
-    maStartDate: any;
-    maEndDate: any;
+  $key: string;
+  sid: string;
+  initials: string;
+  nameEN: string;
+  nameTH: string;
+  productId: string;
+  maLevelId: string;
+  product: Product;
+  maStartDate: any;
+  maEndDate: any;
+  addresses : Addresses[]
 }
 
 export interface MaLevel {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
+}
+
+
+export interface Addresses {
+  street : string;
+  city : string;
+  province : string;
+  zipCode : string;
 }
--- src/app/pages/add-site/add-site.component.ts
+++ src/app/pages/add-site/add-site.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Product} from "../../models/product.model";
+import {Addresses} from "../../models/site.model";
 
 @Component({
   selector: 'app-add-site',
@@ -6,10 +9,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-site.component.css']
 })
 export class AddSiteComponent implements OnInit {
+  siteForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.buildForm()
   }
 
+  private buildForm() {
+    this.siteForm = this.fb.group({
+      initials: [''],
+      nameTH: [''],
+      nameEN: [''],
+      productId: [''],
+      maLevelId: [''],
+      product: [''],
+      addresses: this.fb.group({
+        street: [''],
+        city: [''],
+        province: [''],
+        zipCode: [''],
+      }),
+    })
+  }
+
+
+  get initials() {
+    return this.siteForm.get('initials');
+  }
+
+  get nameTH() {
+    return this.siteForm.get('nameTH');
+  }
+
+  get nameEN() {
+    return this.siteForm.get('nameEN');
+  }
+
+  get productId() {
+    return this.siteForm.get('productId');
+  }
+
+
+
+  addSite() {
+
+  }
+
+  alertCancelAddCustomer() {
+
+  }
 }
